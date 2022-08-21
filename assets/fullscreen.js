52d3afa0ffb7bdf64d97d6e4ce57ae326e75d597
Dylan Subramanian <77794302+dylsub@users.noreply.github.com>
2022-08-21 09:15:50 -0500

    Delete assets directory

--- assets/fullscreen.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-//fullscreen
-const slider = document.querySelector(".slider");
-const fullscreenBtn = document.querySelector(".fullscreen").addEventListener("click", () => {
-    slider.classList.add("slider__fullscreen");
-});
